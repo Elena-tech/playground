fea8b5ab10d66f22ff76bc10d2ec63c286188df1
Elena Petrova <07387909591elena@gmail.com>
2020-03-04 21:52:26 +0000

    Js non-completed

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,2 +1,14 @@
 //= link_tree ../images
 //= link_directory ../stylesheets .css
+// vanilla JS
+var grid = document.querySelector('.grid');
+// initialize with element
+var pckry = new Packery( grid, {
+  // options...
+  itemSelector: '.grid-item'
+});
+
+// initialize with selector string
+var pckry = new Packery('.grid', {
+  // options...
+});
